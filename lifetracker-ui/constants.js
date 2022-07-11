e22cec005e2b049d1dcb4973a9ab2ccca7802534
nkewden <nkewden@gmail.com>
2022-07-11 13:30:24 -0500

    deployed the front end

--- lifetracker-ui/constants.js
+++ lifetracker-ui/constants.js
@@ -1,4 +1,4 @@
-export const PRODUCTION_API_BASE_URL = ""
+export const PRODUCTION_API_BASE_URL = "https://nasradin-lifetracker.herokuapp.com/"
 export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001"
 
-export const API_BASE_URL =  DEVELOPMENT_API_BASE_URL
+export const API_BASE_URL =  PRODUCTION_API_BASE_URL
